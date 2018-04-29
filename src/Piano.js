4229173987f91ccb3aec7d25a6f931ed06533d05
Kevin Qi <kqi@amino.com>
2018-04-29 09:22:15 -0700

    refactor config, render all notes again

--- src/Piano.js
+++ src/Piano.js
@@ -2,65 +2,58 @@
 // noreintegrate active notes
 // noreintegrate startnote endnote
 import React from "react";
-import _ from "lodash";
 
 function Key(props) {
   return (
     <div
       style={{
-        backgroundColor: props.backgroundColor,
-        border: `2px solid ${props.stroke}`,
+        background: props.background,
+        border: props.border,
         position: "absolute",
         top: 0,
-        left: `${props.x}%`,
-        width: `${props.width}%`,
-        height: `${props.height}%`,
-        borderRadius: "0 0 4px 4px"
+        left: props.left,
+        width: props.width,
+        height: props.height,
+        borderRadius: "0 0 4px 4px",
+        zIndex: props.zIndex
       }}
     />
   );
 }
 
-const noteConfig = [
-  {
-    name: "c",
-    offset: 0,
-    isBlackKey: false
-  },
-  {
-    name: "db",
-    offset: 0.55,
-    isBlackKey: true
-  },
-  {
-    name: "d",
-    offset: 1,
-    isBlackKey: false
-  },
-  {
-    name: "eb",
-    offset: 1.8,
-    isBlackKey: true
-  },
-  {
-    name: "e",
-    offset: 2,
-    isBlackKey: false
-  },
-  {
-    name: "f",
-    offset: 3,
-    isBlackKey: false
-  },
-  { name: "gb", offset: 3.5, isBlackKey: true }
-  // blackKeyOffsets = [0.55, 1.8, 3.5, 4.7, 5.85].map(
-];
+function ratioToPercentage(ratio) {
+  return `${ratio * 100}%`;
+}
 
 class Piano extends React.Component {
   static defaultProps = {
+    whiteKeyConfig: {
+      widthRatio: 1,
+      heightRatio: 1,
+      border: "2px solid #999",
+      background: "#fff",
+      zIndex: 0
+    },
     blackKeyConfig: {
       widthRatio: 0.66,
-      heightRatio: 0.66
+      heightRatio: 0.66,
+      border: "2px solid #eee",
+      background: "#555",
+      zIndex: 1
+    },
+    noteConfig: {
+      c: { offset: 0, isBlackKey: false },
+      db: { offset: 0.55, isBlackKey: true },
+      d: { offset: 1, isBlackKey: false },
+      eb: { offset: 1.8, isBlackKey: true },
+      e: { offset: 2, isBlackKey: false },
+      f: { offset: 3, isBlackKey: false },
+      gb: { offset: 3.5, isBlackKey: true },
+      g: { offset: 4, isBlackKey: false },
+      ab: { offset: 4.7, isBlackKey: true },
+      a: { offset: 5, isBlackKey: false },
+      bb: { offset: 5.85, isBlackKey: true },
+      b: { offset: 6, isBlackKey: false }
     }
   };
 
@@ -69,27 +62,39 @@ class Piano extends React.Component {
   };
 
   render() {
-    const notes = ["c", "db", "d", "eb", "e"];
-    const notesToNoteConfigs = _.keyBy(noteConfig, "name");
-    const configs = notes.map(note => notesToNoteConfigs[note]);
+    const notes = [
+      "c",
+      "db",
+      "d",
+      "eb",
+      "e",
+      "f",
+      "gb",
+      "g",
+      "ab",
+      "a",
+      "bb",
+      "b"
+    ];
 
     const numWhiteKeys = 7; // noreintegrate get from mapping over notes
-    // noreintegrate configure this from a config object too
-    const whiteKeyHeight = 100;
-    const whiteKeyWidth = 100 / numWhiteKeys;
-    const blackKeyWidth = whiteKeyWidth * this.props.blackKeyConfig.widthRatio;
-    const blackKeyHeight = 100 * this.props.blackKeyConfig.heightRatio;
+    const whiteKeyWidth = 1 / numWhiteKeys;
     return (
       <div style={{ position: "relative", width: "100%", height: "100%" }}>
-        {configs.map(config => {
+        {notes.map(note => {
+          const noteConfig = this.props.noteConfig[note];
+          const keyConfig = noteConfig.isBlackKey
+            ? this.props.blackKeyConfig
+            : this.props.whiteKeyConfig;
           return (
             <Key
-              x={config.offset * whiteKeyWidth}
-              key={config.name}
-              width={config.isBlackKey ? blackKeyWidth : whiteKeyWidth}
-              height={config.isBlackKey ? blackKeyHeight : whiteKeyHeight}
-              stroke={config.isBlackKey ? "#eee" : "#999"}
-              backgroundColor={config.isBlackKey ? "#555" : "#fff"}
+              left={ratioToPercentage(noteConfig.offset * whiteKeyWidth)}
+              width={ratioToPercentage(keyConfig.widthRatio * whiteKeyWidth)}
+              height={ratioToPercentage(keyConfig.heightRatio)}
+              border={keyConfig.border}
+              background={keyConfig.background}
+              zIndex={keyConfig.zIndex}
+              key={note}
             />
           );
         })}
