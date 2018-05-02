e5c5e602d7a81ac6e5cf99e72996ee4362a5c94b
Kevin Qi <kqi@amino.com>
2018-05-01 22:41:35 -0700

    move helpers to separate file

--- src/Piano.js
+++ src/Piano.js
@@ -1,38 +1,10 @@
 // noreintegrate handle null flats/sharps
 import React from 'react';
 import _ from 'lodash';
+import { noteToMidiNumber, getMidiNumberAttributes } from './midiHelpers';
 
 function ratioToPercentage(ratio) {
   return `${ratio * 100}%`;
-}
-
-// TODO: rewrite clearly
-function midiNumberToFrequency(number) {
-  const A4 = 440;
-  return A4 / 32 * Math.pow(2, (number - 9) / 12);
-}
-
-// TODO: move to separate file
-const NOTE_ARRAY = ['c', 'db', 'd', 'eb', 'e', 'f', 'gb', 'g', 'ab', 'a', 'bb', 'b'];
-const MIDI_NUMBER_C0 = 12;
-const NOTE_REGEX = /(\w+)(\d)/;
-const NOTES_IN_OCTAVE = 12;
-function noteToMidiNumber(note) {
-  const [, basenote, octave] = NOTE_REGEX.exec(note);
-  const offset = NOTE_ARRAY.indexOf(basenote);
-  return MIDI_NUMBER_C0 + offset + NOTES_IN_OCTAVE * parseInt(octave, 10);
-}
-function getMidiNumberAttributes(number) {
-  const offset = (number - MIDI_NUMBER_C0) % NOTES_IN_OCTAVE;
-  const octave = Math.floor((number - MIDI_NUMBER_C0) / NOTES_IN_OCTAVE);
-  const basenote = NOTE_ARRAY[offset];
-  return {
-    note: `${basenote}${octave}`,
-    basenote,
-    octave,
-    midiNumber: number,
-    frequency: midiNumberToFrequency(number),
-  };
 }
 
 // noreintegrate refactor
