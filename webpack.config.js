c6a269770f2b63eb613ef69d90f32c6f9a0da1e5
Kevin Qi <iqnivek@gmail.com>
2018-06-15 22:15:16 -0700

    tweak devtool and build commands

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  devtool: 'source-map',
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'build'),
