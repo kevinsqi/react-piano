4623b05a41fecb7cb0a9d4cdea07360cac280879
Kevin Qi <iqnivek@gmail.com>
2018-07-01 19:06:04 -0700

    have Oscillator take midinumbers instead of frequencies, remove freq from midiNumberAttributes

--- demo/src/Oscillator.js
+++ demo/src/Oscillator.js
@@ -1,4 +1,22 @@
-// TODO: dedupe with microharmonic
+import range from 'lodash.range';
+
+export const MIN_MIDI_NUMBER = 12;
+export const MAX_MIDI_NUMBER = 127;
+
+// TODO: rewrite more understandably
+function midiNumberToFrequency(midiNumber) {
+  const A4 = 440;
+  return A4 / 32 * Math.pow(2, (midiNumber - 9) / 12);
+}
+
+const midiNumberFrequencies = range(MIN_MIDI_NUMBER, MAX_MIDI_NUMBER + 1).reduce(
+  (cache, midiNumber) => {
+    cache[midiNumber] = midiNumberToFrequency(midiNumber);
+    return cache;
+  },
+  {},
+);
+
 class Oscillator {
   constructor(options = {}) {
     this.audioContext = options.audioContext;
@@ -9,27 +27,27 @@ class Oscillator {
     this.oscillators = {};
   }
 
-  start(freq) {
-    if (this.oscillators[freq]) {
+  start(midiNumber) {
+    if (this.oscillators[midiNumber]) {
       return;
     }
 
     let oscillator = this.audioContext.createOscillator();
     oscillator.type = 'sine';
-    oscillator.frequency.value = freq;
+    oscillator.frequency.value = midiNumberFrequencies[midiNumber];
     oscillator.connect(this.gainNode);
     oscillator.start(0);
-    this.oscillators[freq] = oscillator;
+    this.oscillators[midiNumber] = oscillator;
   }
 
-  stop(freq) {
-    if (!this.oscillators[freq]) {
+  stop(midiNumber) {
+    if (!this.oscillators[midiNumber]) {
       return;
     }
 
-    this.oscillators[freq].stop(0);
-    this.oscillators[freq].disconnect();
-    delete this.oscillators[freq];
+    this.oscillators[midiNumber].stop(0);
+    this.oscillators[midiNumber].disconnect();
+    delete this.oscillators[midiNumber];
   }
 }
 
--- src/midiHelpers.js
+++ src/midiHelpers.js
@@ -8,12 +8,6 @@ export const MAX_MIDI_NUMBER = 127;
 const NOTE_REGEX = /(\w+)(\d)/;
 const NOTES_IN_OCTAVE = 12;
 
-// TODO: rewrite more understandably
-function midiNumberToFrequency(number) {
-  const A4 = 440;
-  return A4 / 32 * Math.pow(2, (number - 9) / 12);
-}
-
 function buildMidiNumberAttributes(number) {
   const offset = (number - MIDI_NUMBER_C0) % NOTES_IN_OCTAVE;
   const octave = Math.floor((number - MIDI_NUMBER_C0) / NOTES_IN_OCTAVE);
@@ -23,7 +17,6 @@ function buildMidiNumberAttributes(number) {
     basenote,
     octave,
     midiNumber: number,
-    frequency: midiNumberToFrequency(number),
     isAccidental: ACCIDENTALS.includes(basenote),
   };
 }
