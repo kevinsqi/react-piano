b87c9cb0db55ab0235c9dc1da6db067537f5a5de
Kevin Qi <iqnivek@gmail.com>
2018-07-20 21:45:10 -0700

    implement fromNote which converts from pitch notation to midi number, cover flats/sharps

--- src/MidiNumbers.js
+++ src/MidiNumbers.js
@@ -1,11 +1,30 @@
 import range from 'lodash.range';
 
 const BASENOTES = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
+const BASENOTE_NUMBERS = {
+  C: 0,
+  'C#': 1,
+  Db: 1,
+  D: 2,
+  'D#': 3,
+  Eb: 3,
+  E: 4,
+  F: 5,
+  'F#': 6,
+  Gb: 6,
+  G: 7,
+  'G#': 8,
+  Ab: 8,
+  A: 9,
+  'A#': 10,
+  Bb: 10,
+  B: 11,
+};
 const ACCIDENTALS = ['Db', 'Eb', 'Gb', 'Ab', 'Bb'];
 const MIDI_NUMBER_C0 = 12;
 const MIN_MIDI_NUMBER = MIDI_NUMBER_C0;
 const MAX_MIDI_NUMBER = 127;
-const NOTE_REGEX = /(\w+)(\d)/;
+const NOTE_REGEX = /([a-g])([#b]?)(\d+)/;
 const NOTES_IN_OCTAVE = 12;
 
 function buildMidiNumberAttributes(midiNumber) {
@@ -30,12 +49,27 @@ function buildMidiNumberAttributesCache() {
 
 const midiNumberAttributesCache = buildMidiNumberAttributesCache();
 
-// Notes are strings in the format "[basenote][octave]", e.g. "a4", "cb7"
-// Converts note to midi number, as specified in:
-// https://www.midikits.net/midi_analyser/midi_note_numbers_for_octaves.htm
-function noteToMidiNumber(note) {
-  const [, basenote, octave] = NOTE_REGEX.exec(note);
-  const offsetFromC = BASENOTES.indexOf(basenote);
+// Converts string notes in scientific pitch notation to a MIDI number, or null if not a note.
+//
+// Example: "c#0" => 13, "eb5" => 75, "abc" => null
+//
+// References:
+// - http://www.flutopedia.com/octave_notation.htm
+// - https://github.com/danigb/tonal/blob/master/packages/note/index.js
+function fromNote(note) {
+  if (!note) {
+    return null;
+  }
+  const match = NOTE_REGEX.exec(note.toLowerCase());
+  if (!match) {
+    return null;
+  }
+  const [, letter, accidental, octave] = match;
+  const basenote = `${letter.toUpperCase()}${accidental}`;
+  const offsetFromC = BASENOTE_NUMBERS[basenote];
+  if (!offsetFromC) {
+    return null;
+  }
   return MIDI_NUMBER_C0 + offsetFromC + NOTES_IN_OCTAVE * parseInt(octave, 10);
 }
 
@@ -52,6 +86,7 @@ const NATURAL_MIDI_NUMBERS = range(MIN_MIDI_NUMBER, MAX_MIDI_NUMBER + 1).filter(
 );
 
 export default {
+  fromNote,
   getAttributes,
   MIN_MIDI_NUMBER,
   MAX_MIDI_NUMBER,
--- src/MidiNumbers.test.js
+++ src/MidiNumbers.test.js
@@ -11,3 +11,14 @@ test('getAttributes', () => {
     midiNumber: 12,
   });
 });
+
+test('fromNote', () => {
+  expect(MidiNumbers.fromNote('c#0')).toBe(13);
+  expect(MidiNumbers.fromNote('eb5')).toBe(75);
+  expect(MidiNumbers.fromNote('G4')).toBe(67);
+
+  // Invalid notes
+  expect(MidiNumbers.fromNote('fb1')).toBe(null);
+  expect(MidiNumbers.fromNote('')).toBe(null);
+  expect(MidiNumbers.fromNote(null)).toBe(null);
+});
