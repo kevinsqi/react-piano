8407306837b7f4f53d0aa9e14f9068a86d88cc7b
Kevin Qi <iqnivek@gmail.com>
2019-12-30 22:09:34 -0800

    Updates

---
+++ precache-manifest.c8e3aa5ed41fc5fa94a8b41f033ee2f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f25f4086012b16c9456df855b3dee2b",
+    "url": "/react-piano/index.html"
+  },
+  {
+    "revision": "8c53e015677b1fd93078",
+    "url": "/react-piano/static/css/2.1e78366a.chunk.css"
+  },
+  {
+    "revision": "31ece3b523c44699b518",
+    "url": "/react-piano/static/css/main.8bb316ed.chunk.css"
+  },
+  {
+    "revision": "8c53e015677b1fd93078",
+    "url": "/react-piano/static/js/2.e55574c6.chunk.js"
+  },
+  {
+    "revision": "d1e6d2e90fc3576c54a78df6d7f1b3b6",
+    "url": "/react-piano/static/js/2.e55574c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31ece3b523c44699b518",
+    "url": "/react-piano/static/js/main.4689a5ea.chunk.js"
+  },
+  {
+    "revision": "d03279d92ea4b26af533",
+    "url": "/react-piano/static/js/runtime-main.9c47d169.js"
+  }
+]);
