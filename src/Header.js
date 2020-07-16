2370f612525c2b88337adc174882b72904b9dad6
Josh Jacobson <joshjacobson14@gmail.com>
2020-07-15 17:40:55 -0700

    Update title

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ function Header() {
     <div style={{ background: '#333' }}>
       <div className="container">
         <div className="text-sm-center text-white py-5">
-          <h1>react-piano</h1>
+          <h1>Lambda Piano Demo</h1>
           <p>
             An interactive piano keyboard for React. Supports custom sounds,
             <br className="d-none d-sm-block" /> touch/click/keyboard events, and fully configurable
